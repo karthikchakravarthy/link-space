c724cfef338c5a437c26d009b87b7b4d7c436ae5
karthik chakravarthy <karthikchakry@gmail.com>
2020-06-06 16:20:39 +0530

    giving password match validation in sign up page

--- src/components/login.card/LoginCard.js
+++ src/components/login.card/LoginCard.js
@@ -5,71 +5,80 @@ import HelpText from "../help.text/HelpText"
 import "./LoginCard.css"
 
 function LoginCard(props) {
-  const [login, setLogin] = useState(true);
+  const [login, setLogin] = useState(true),
+  options = {};
 
-    function onSubmit(e){
-      e.preventDefault();
-      var inputs = document.querySelectorAll('.validate-input input')
-      var check = true;
+  function onSubmit(e){
+    e.preventDefault();
+    var inputs = document.querySelectorAll('.validate-input input')
+    var check = true;
 
-      for(var i=0; i<inputs.length; i++) {
-          if(validate(inputs[i]) === false){
-              showValidate(inputs[i]);
-              check=false;
-          }
+    for(var i=0; i<inputs.length; i++) {
+      if(inputs[i].name === 'pass') {
+        options.pass = inputs[i];
       }
-      if(check){
-        props.setAuthentication(true);
+      if(inputs[i].name === 'verifypass') {
+        options.verifypass = inputs[i];
       }
-
+      if(validate(inputs[i]) === false){
+        showValidate(inputs[i]);
+        check=false;
+      }
     }
 
-
-    function onFocus(e){
-      hideValidate(e.target);
+    if(options.verifypass && (options.pass.value !== options.verifypass.value)){
+      check=false;
+      showValidate(options.verifypass);
     }
-
-    function hideValidate(input) {
-        var thisAlert = input.parentElement;
-        thisAlert.classList.remove('alert-validate');
+    
+    if(check){
+      props.setAuthentication(true);
     }
+  }
 
-    function validate (input) {
-      if(input.type === 'email' || input.name === 'email') {
-          if(input.value.trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
-              return false;
-          }
+  function onFocus(e){
+    hideValidate(e.target);
+  }
+
+  function hideValidate(input) {
+    var thisAlert = input.parentElement;
+    thisAlert.classList.remove('alert-validate');
+  }
+
+  function validate (input) {
+    if(input.type === 'email' || input.name === 'email') {
+      if(input.value.trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+        return false;
       }
-      else {
-          if(input.value.trim() === ''){
-              return false;
-          }
+    }
+    else {
+      if(input.value.trim() === ''){
+        return false;
       }
+    }
   }
 
   function showValidate(input) {
-      var thisAlert = input.parentElement;
-
-      thisAlert.classList.add('alert-validate');
+    var thisAlert = input.parentElement;
+    thisAlert.classList.add('alert-validate');
   }
-    
 
   return (
-        <div className="login-box">
-            <form className="login-form" onSubmit = {onSubmit} >
-                <div className="company-name">Link Space</div>
+    <div className="login-box">
+      <form className="login-form" onSubmit = {onSubmit} >
+        <div className="company-name">Link Space</div>
 
-                <LoginInput onFocus= {onFocus} data={{type:'text', name:'email', placeHolder: 'Email', validationMsg: "Valid email is: a@b.c"}}/>
-                <LoginInput onFocus= {onFocus} data={{type:'password', name:'pass', placeHolder: 'Password', validationMsg: "Enter password"}}/>
-                {!login && <LoginInput onFocus= {onFocus} data={{type:'password', name:'pass', placeHolder: 'Re Enter Password', validationMsg: "Re Enter password"}}/>}
+        <LoginInput onFocus= {onFocus} data={{type:'text', name:'email', placeHolder: 'Email', validationMsg: "Valid email is: a@b.c"}}/>
+        <LoginInput onFocus= {onFocus} data={{type:'password', name:'pass', placeHolder: 'Password', validationMsg: "Enter password"}}/>
+        {!login && <LoginInput onFocus= {onFocus} data={{type:'password', name:'verifypass', placeHolder: 'Re Enter Password', validationMsg: " Password doesn't match"}}/>}
 
-                {login && <FormButton  displayName='login' />}
-                {!login && <FormButton displayName='sign up' />}
+        {login && <FormButton  displayName='login' />}
+        {!login && <FormButton displayName='sign up' />}
 
-                {login &&<HelpText login={true} setLogin={setLogin} question='Don’t have an account?' answer='Sign Up' />}
-                {!login &&<HelpText login={false} setLogin={setLogin} question='Already have an account?' answer='Sign In' />}
-            </form>
-        </div>
+        {login &&<HelpText login={true} setLogin={setLogin} question='Don’t have an account?' answer='Sign Up' />}
+        {!login &&<HelpText login={false} setLogin={setLogin} question='Already have an account?' answer='Sign In' />}
+      </form>
+    </div>
   );
 }
 
