65f1bc042a9269f66d0c3bfcf472b818d8fa9cf5
karthik chakravarthy <karthikchakry@gmail.com>
2021-05-03 11:18:41 +0530

    changes made to styling related  to card

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,7 +4,6 @@ import "./Card.css"
 function Card({link}) {
     return (
         <div className='card'>
-            <span className='photo'></span>
             <h3>{link.name}</h3>
             <a href={link.link}>{link.link}</a>
         </div>
--- src/linksData.js
+++ src/linksData.js
@@ -2,53 +2,53 @@ export const linksData = {
     "links": [
         {
             "id": "1",
-            "name": "google",
-            "link": "google.com"
+            "name": "Google",
+            "link": "https://www.google.com"
         },
         {
             "id": "2",
-            "name": "facebook",
-            "link": "facebook.com"
+            "name": "Facebook",
+            "link": "https://www.facebook.com"
         },
         {
             "id": "3",
-            "name": "instagram",
-            "link": "instagram.com"
+            "name": "Instagram",
+            "link": "https://www.instagram.com"
         },
         {
             "id": "4",
-            "name": "udemy",
-            "link": "udemy.com"
+            "name": "Udemy",
+            "link": "https://www.udemy.com"
         },
         {
             "id": "5",
-            "name": "gmail",
-            "link": "gmail.com"
+            "name": "Gmail",
+            "link": "https://www.gmail.com"
         },
         {
             "id": "6",
-            "name": "google",
-            "link": "google.com"
+            "name": "Google",
+            "link": "https://www.google.com"
         },
         {
             "id": "7",
-            "name": "facebook",
-            "link": "facebook.com"
+            "name": "Facebook",
+            "link": "https://www.facebook.com"
         },
         {
             "id": "8",
-            "name": "instagram",
-            "link": "instagram.com"
+            "name": "Instagram",
+            "link": "https://www.instagram.com"
         },
         {
             "id": "9",
-            "name": "udemy",
-            "link": "udemy.com"
+            "name": "Udemy",
+            "link": "https://www.udemy.com"
         },
         {
             "id": "10",
-            "name": "gmail",
-            "link": "gmail.com"
+            "name": "Gmail",
+            "link": "https://www.gmail.com"
         }
     ]
 }
