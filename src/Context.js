cef204a6bde104805dc5e7c935ccc4ff464c5b70
karthik chakravarthy <karthikchakry@gmail.com>
2021-05-05 16:26:19 +0530

    connecting to the server instead of static data in the app

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,15 @@
-import React, {useState} from 'react'
-import {linksData} from "./linksData"
+import React, {useState, useEffect} from 'react'
 
 const Context = React.createContext()
 
 function ContextProvider({children}) {
-    const [allLinks, setAllLinks] = useState(linksData.links)
+    const [allLinks, setAllLinks] = useState([])
+
+    useEffect(()=> {
+        fetch('http://localhost:3050/api/links')
+        .then(res => res.json())
+        .then(data => setAllLinks(data))
+    }, [])
     return (
         <Context.Provider value={{allLinks}}>
             {children}
