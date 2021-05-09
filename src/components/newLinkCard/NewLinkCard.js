894affccc393df1722a8bef24537bb4239516a14
Jaiveer Biradar <jaiveer.biradar@capgemini.com>
2021-05-09 20:01:55 +0530

    implemented add, edit and delete link

--- src/components/newLinkCard/NewLinkCard.js
+++ src/components/newLinkCard/NewLinkCard.js
@@ -1,8 +1,11 @@
 import React, { useContext, useState } from "react"
 import { Context } from "../../Context"
 import "./NewLinkCard.css"
+import {Button, TextField} from '@material-ui/core';
 
-function NewLinkCard() {
+import Popover from '../popOver/PopOver';
+
+function NewLinkCard(props) {
   const [newLink, setnewLink] = useState({ name: '', link: '' });
   const { allLinks, setAllLinks, setShowNewLink } = useContext(Context);
 
@@ -32,6 +35,7 @@ function NewLinkCard() {
       .then((response) => {
         if (response.ok) {
           setShowNewLink(false);
+          props.onClose && props.onClose();//to close the drawer when app in mobile
           response.json().then(newLink => {
             setAllLinks([newLink, ...allLinks]);
           })
@@ -42,27 +46,35 @@ function NewLinkCard() {
   }
   return (
     <div className='card'>
-      <label> Name:
-            <input
-          type='text'
-          name='name'
-          value={newLink.name}
-          onChange={onChangeHandler}
-        />
-      </label>
-      <label> Url:
-            <input
-          type='text'
-          name='link'
-          value={newLink.link}
-          onChange={onChangeHandler}
-        />
-      </label>
-      <input
-        type='button'
-        value='Add Link'
-        onClick={addNewLinkHandler}
-      />
+      <div className='grid-container'>
+         <div className='alignCenter'> 
+           <label> Name: </label>
+         </div>
+         <div>   
+           <TextField  label="Name" variant="outlined" size='small'
+             name='name' value={newLink.name} onChange={onChangeHandler}
+           />
+         </div> 
+         <div className='alignCenter'>
+           <label> Url: </label>
+         </div> 
+         <div>   
+          <TextField  label="Link" variant="outlined" size='small' fullWidth={true}
+             name='link' value={newLink.link} onChange={onChangeHandler}
+          />
+         </div>
+      </div> 
+      <div className='btnContainer'>
+           <Button style={{marginRight: 10}} variant="outlined" onClick={()=>{
+             setShowNewLink(false);
+             props.onClose && props.onClose();//to close the drawer when app in mobile
+           }}>
+              Cancel
+           </Button>
+           <Button variant="contained" onClick={addNewLinkHandler}>
+              Add Link
+           </Button>
+      </div>
     </div>
   )
 }
